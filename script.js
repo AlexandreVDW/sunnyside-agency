766d394b84757cce6f3976dd28b3b67d466dbc9c
AlexandreVDW <vandewielealexandre@gmail.com>
2023-09-14 11:15:57 +0200

    section 1 menu hamburger fail :(

---
+++ script.js
@@ -0,0 +1,11 @@
+const hamburger = document.querySelector(".section_one_div_hamburger");
+const navMenu = document.querySelector(".section_one_navbar_menu");
+
+hamburger.addEventListener("click", ()=>{
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+})
+document.querySelectorAll(".section_one_navbar_item_link").forEach(n=> n.addEventListener("click", ()=> {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}))
